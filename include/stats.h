269fc556dc722fb58bde4622582d9d7041a500c4
Luca <luca.valente@unibo.it>
2024-02-21 11:52:02 +0100

    Adopt stats defines everywhere

--- include/stats.h
+++ include/stats.h
@@ -10,7 +10,7 @@
   volatile long unsigned levent_start      = read_csr(0xC05); \
   volatile long unsigned sevent_start      = read_csr(0xC06); \
   volatile long unsigned ifempty_start     = read_csr(0xC07); \
-  volatile long unsigned stall_start       = read_csr(0xC08);
+  volatile long unsigned stall_start       = read_csr(0xC08)
 
 #define STOP_STATS() \
   volatile long unsigned cycles_cntr = cycles_cntr_start - read_csr(cycle);       \
@@ -20,7 +20,7 @@
   volatile long unsigned levent      = levent_start      - read_csr(0xC05); \
   volatile long unsigned sevent      = sevent_start      - read_csr(0xC06); \
   volatile long unsigned ifempty     = ifempty_start     - read_csr(0xC07); \
-  volatile long unsigned stall       = stall_start       - read_csr(0xC08);
+  volatile long unsigned stall       = stall_start       - read_csr(0xC08)
 
 #define PRINT_STATS() \
-  printf("%lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu\n", cycles_cntr, instrs_cntr, icachemiss, dcachemiss, levent    , sevent    , ifempty   , stall   );
+  printf("%lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu\n", cycles_cntr, instrs_cntr, icachemiss, dcachemiss, levent    , sevent    , ifempty   , stall   )
