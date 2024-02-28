c39ebb36bb80393658c18e3b088bf0e5839f090b
Luca <luca.valente@unibo.it>
2024-02-28 20:30:19 +0100

    Fix `stats.h`

--- include/stats.h
+++ include/stats.h
@@ -4,23 +4,11 @@
 
 #define START_STATS() \
   volatile long unsigned cycles_cntr_start = read_csr(cycle);       \
-  volatile long unsigned instrs_cntr_start = read_csr(instret);     \
-  volatile long unsigned icachemiss_start  = read_csr(0xC03); \
-  volatile long unsigned dcachemiss_start  = read_csr(0xC04); \
-  volatile long unsigned levent_start      = read_csr(0xC05); \
-  volatile long unsigned sevent_start      = read_csr(0xC06); \
-  volatile long unsigned ifempty_start     = read_csr(0xC07); \
-  volatile long unsigned stall_start       = read_csr(0xC08)
+  volatile long unsigned instrs_cntr_start = read_csr(instret);     
 
 #define STOP_STATS() \
-  volatile long unsigned cycles_cntr = cycles_cntr_start - read_csr(cycle);       \
-  volatile long unsigned instrs_cntr = instrs_cntr_start - read_csr(instret);     \
-  volatile long unsigned icachemiss  = icachemiss_start  - read_csr(0xC03); \
-  volatile long unsigned dcachemiss  = dcachemiss_start  - read_csr(0xC04); \
-  volatile long unsigned levent      = levent_start      - read_csr(0xC05); \
-  volatile long unsigned sevent      = sevent_start      - read_csr(0xC06); \
-  volatile long unsigned ifempty     = ifempty_start     - read_csr(0xC07); \
-  volatile long unsigned stall       = stall_start       - read_csr(0xC08)
+  volatile long unsigned cycles_cntr = read_csr(cycle) - cycles_cntr_start;       \
+  volatile long unsigned instrs_cntr = read_csr(instret) - instrs_cntr_start;     
 
 #define PRINT_STATS() \
-  printf("%lu, %lu, %lu, %lu, %lu, %lu, %lu, %lu\n", cycles_cntr, instrs_cntr, icachemiss, dcachemiss, levent    , sevent    , ifempty   , stall   )
+  printf("%lu, %lu \n", cycles_cntr, instrs_cntr)
